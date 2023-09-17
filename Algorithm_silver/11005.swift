34f726177e9062281c07a76a8d1bc52cf33a1d00
GodSamble <osgneb8359@naver.com>
2023-09-17 16:52:38 +0900

    2023/9/17 commit

---
+++ Algorithm_silver/11005.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by 고영민 on 2023/09/17.
+//
+
+import Foundation
+
+//11005
+
+let input: [Substring] = readLine()!.split(separator: " ")
+print(String(Int(input[0])!, radix: Int(input[1])!).uppercased())
---
+++ Algorithm_silver/2745.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by 고영민 on 2023/09/17.
+//
+
+import Foundation
+
+let input = readLine()!.split(separator: " ")
+
+
+if let result = Int(input[0], radix: Int(input[1])!) {
+    print(result)
+}
