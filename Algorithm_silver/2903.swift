211f40d52137ea5f16b30b534a86de5ec94b376c
GodSamble <osgneb8359@naver.com>
2023-09-18 13:00:40 +0900

    2023/9/18 commit

---
+++ Algorithm_silver/2903.swift
@@ -0,0 +1,15 @@
+//
+//  2903.swift
+//  Algorithm
+//
+//  Created by 고영민 on 2023/09/18.
+//
+
+import Foundation
+
+let n = Int(readLine()!)!
+
+let num = pow(2, Double(n)) + 1
+let result = Int(pow(num, 2))
+
+print(result)
--- Algorithm_silver/main.swift
+++ Algorithm_silver/main.swift
@@ -1,15 +1,1 @@
-//
-//  2903.swift
-//  Algorithm
-//
-//  Created by 고영민 on 2023/09/18.
-//
 
-import Foundation
-
-let n = Int(readLine()!)!
-
-
-print(num)
-
-////let num = (2**n+1)**2
