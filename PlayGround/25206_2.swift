d0a8c45cfec73f45110cfc771610e8d984174f4e
GodSamble <osgneb8359@naver.com>
2023-11-01 02:09:54 +0900

    2023/11/1 (복습 목적의 재풀이) commit

---
+++ PlayGround/25206_2.swift
@@ -0,0 +1,27 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by 고영민 on 11/1/23.
+//
+
+import Foundation
+// MARK: 너의 시험은
+
+let dict: [String: Double] = [
+    "A+": 4.5, "A0": 4.0, "B+": 3.5, "B0": 3.0,
+    "C+": 2.5, "C0": 2.0, "D+": 1.5, "D0": 1.0, "F": 0
+]
+
+var totalScore : Double = 0
+var totalGrade : Double = 0
+
+for _ in 0..<20 {
+    var input = readLine()!.split(separator: " ").map{String($0)}
+    if let score = dict[input[2]] {
+        totalGrade += Double(input[1])! * score
+        totalScore += Double(input[1])!
+    }
+}
+
+print(totalGrade/totalScore)
